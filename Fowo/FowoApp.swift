4e469f91b0ea4608fc95d51ecb8dd01347dd223d
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2022-10-04 22:33:45 +0700

    initial commit

---
+++ Fowo/FowoApp.swift
@@ -0,0 +1,17 @@
+//
+//  FowoApp.swift
+//  Fowo
+//
+//  Created by Christianto Budisaputra on 04/10/22.
+//
+
+import SwiftUI
+
+@main
+struct FowoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
