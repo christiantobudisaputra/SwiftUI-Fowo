b84bfe4a6156c8f3dda19f4f67e53111428f3403
Christianto Budisaputra <53547157+chrsomle@users.noreply.github.com>
2022-10-04 22:39:40 +0700

    add local asset

---
+++ Fowo/Home/Components/PostView.swift
@@ -0,0 +1,23 @@
+//
+//  PostView.swift
+//  Fowo
+//
+//  Created by Christianto Budisaputra on 04/10/22.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostView()
+            .padding()
+            .previewLayout(.sizeThatFits)
+            .preferredColorScheme(.dark)
+    }
+}
